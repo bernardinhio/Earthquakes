7f982070b551ba79ea06c77adf9b66bf49fbc04c
Bernard Karam <bernard.karam@sport1.de>
2019-02-16 18:33:25 +0100

    Create retrofit call from Activity and receive data, prepare the data structure for recyclerView for results, add menu button start the call

---
+++ app/src/main/java/bernardo/bernardinhio/retrofitgetdataearthquakeapi/view/EarthquakeDataModel.kt
@@ -0,0 +1,8 @@
+package bernardo.bernardinhio.retrofitgetdataearthquakeapi.view
+
+data class EarthquakeDataModel(
+        val place : String? = "",
+        val time : String? = "",
+        val title : String? = ""
+){
+}
--- app/src/main/java/bernardo/bernardinhio/retrofitgetdataearthquakeapi/view/MainActivity.java
+++ app/src/main/java/bernardo/bernardinhio/retrofitgetdataearthquakeapi/view/MainActivity.java
@@ -2,8 +2,21 @@ package bernardo.bernardinhio.retrofitgetdataearthquakeapi.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import bernardo.bernardinhio.retrofitgetdataearthquakeapi.R;
+import bernardo.bernardinhio.retrofitgetdataearthquakeapi.model.EarthquakesRoot;
+import bernardo.bernardinhio.retrofitgetdataearthquakeapi.service.RetrofitCalls;
+import bernardo.bernardinhio.retrofitgetdataearthquakeapi.service.RetrofitInstance;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +25,84 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m){
+        super.onCreateOptionsMenu(m);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, m);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        switch (itemId){
+            case R.id.show_result:
+                getDataFromApiUrl();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void getDataFromApiUrl() {
+        RetrofitCalls retrofitCalls
+                = RetrofitInstance.INSTANCE.setupRetrofitCalls();
+
+        Call<EarthquakesRoot> call
+                = retrofitCalls.getEarthquakes();
+
+        call.enqueue(new Callback<EarthquakesRoot>() {
+            @Override
+            public void onResponse(Call<EarthquakesRoot> call, Response<EarthquakesRoot> response) {
+
+                // get the returned Object from the API
+                EarthquakesRoot earthquakesRoot
+                        = response.body();
+
+                EarthquakesRoot.Features[] features = earthquakesRoot.getFeatures();
+
+                ArrayList<EarthquakeDataModel> arrayListEarthquakes = new ArrayList<EarthquakeDataModel>();
+                String place;
+                String time;
+                String title;
+
+                if (features != null) {
+                    for (int i = 0; i< features.length; i++){
+                        place = features[i].getProperties().getPlace();
+                        time = features[i].getProperties().getTime();
+                        title = features[i].getProperties().getTitle();
+
+                        arrayListEarthquakes.add(new EarthquakeDataModel(
+                                place,
+                                time,
+                                title
+                        ));
+                    }
+                }
+
+
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+                // set RecyclerView
+                setupRecyclerView(arrayListEarthquakes);
+
+            }
+
+            @Override
+            public void onFailure(Call<EarthquakesRoot> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "API call failed!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+    private void setupRecyclerView(ArrayList<EarthquakeDataModel> arrayListEarthquakes) {
+
+    }
+
 }
